9fdd478e83a718a2997f91d3a30f019859173df9
Michelle Poterek <poterekm@gmail.com>
2017-07-04 19:32:33 -0700

    adjust path to Contact

--- app/app.php
+++ app/app.php
@@ -1,31 +1,24 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Contacts.php";
-
-
+    require_once __DIR__."/../src/Contact.php";
     session_start();
+
     if(empty($_SESSION['list_of_contacts'])) {
         $_SESSION['list_of_contacts'] = array();
-
-
-
-
+    }
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
-
     $app->get("/", function() use ($app) {
         return $app['twig']->render('home.html.twig', array('contacts' => Contact::getAll()));
     });
-
     $app->post("/new_contact_display", function() use($app) {
         $contact = new Contact($_POST['name'], $_POST['number'], $_POST['address'], $_POST['image']);
         $contact->save();
         return $app['twig']->render('new_contact_display.html.twig', array('contacts' => $contact));
     });
-
     $app->get("/search", function() use ($app){
         $contacts = Contact::getAll();
         $contacts_matching_search = array();
@@ -38,11 +31,8 @@
         }
         return $app['twig']->render('search.html.twig', array('contacts' => $contacts_matching_search));
     });
-
     $app->post("/delete", function() use($app){
         return $app['twig']->render('delete_all.html.twig', array('contacts' => Contact::deleteAll()));
     });
-
-
     return $app;
 ?>
